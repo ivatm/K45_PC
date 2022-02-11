a3bf1e531c559398fb96534a2d188a6bb6dfd5fd
Ivashchenko <aivashchenko@lan.hzm.de>
2022-02-11 15:59:49 +0200

    Current stage - almost implemented

--- src/K45Unit.py
+++ src/K45Unit.py
@@ -6,50 +6,35 @@ Created on 23 жовт. 2021
 #from pickle import TRUE
 #from ecdsa.test_malformed_sigs import params
 #from Tools.pynche.StripViewer import constant
+from pickle import FALSE
+import time
+import serial
+from xml.sax import _false
 
-class TemperatureValue(object):
-    '''
-    The temperature value
-    '''
-    T = 20
-    CelsOrKelv = True
-    CONSTANT_Shift = 273.15
-    
-    def __init__(self, Value, UnitCOrK):
-        '''
-        Constructor for temperature value
-        '''
-        self.T = Value
-        self.CelsOrKelv = UnitCOrK
-
-    def GetKelvinValue(self):
-        '''
-        Returns value in Kelvin
-        '''
-        return self.T
-
-    def GetCelsiumValue(self):
-        '''
-        Returns value in Celsium
-        '''
-        return self.T - self.CONSTANT_Shift
 
 class K45_Unit(object):
     '''
     Object of K45 
     '''
-    SetOrScanState = True
-    CelseOrKelvin = True
-    CryoLiquidesLevelMeasureOn = True
-    Treal    = TemperatureValue(20, True)
-    Tset     = TemperatureValue(20, True)
-    Tcur_set = TemperatureValue(20, True)
+    SetOrScanState = False
+    CelseOrKelvin = False
+    CryoLiquidesLevelMeasureOn = False
+    
+    ReadBufferLength = 25
     D_T      = 1   # K/C
     D_t      = 0.1 # mS
-    Ureal    = 10e-6  # V
     Kprop    = 10
     Kdiff    = 10
+    
+    Ureal    = 10e-6  # V
+    Treal    = 20
+    Tset     = 20
+    Tcur_set = 20
     L_Level  = 90 # %
+    
+    CoProcessorState = 0
+    # ------------------------------------------------------------------------------------
+    UnitEvailable = False
 
     def __init__(self, SetOrScanState, CelseOrKelvin , CryoLiquidesLevelMeasureOn):
         '''
@@ -58,3 +43,173 @@ class K45_Unit(object):
         self.SetOrScanState = SetOrScanState
         self.CelseOrKelvin  = CelseOrKelvin
         self.CryoLiquidesLevelMeasureOn = CryoLiquidesLevelMeasureOn
+    
+# -----------------------------------------------------------------------------------
+
+    def receivedProcessing(self, inBuff):
+        beg = ''
+        for x in range(len(inBuff[:3])):
+            beg += chr(inBuff[x])
+        
+        end = ''
+        for x in range(len(inBuff[22:])):
+            end += chr(inBuff[22 + x])
+    
+        
+        if (len(inBuff) != self.ReadBufferLength) or beg != "beg" or  end != "end":
+            print("Length = " + str(len(inBuff)))
+            
+            beg = ''
+            for x in range(len(inBuff[:3])):
+                beg += chr(inBuff[x])
+            
+            print("beg = " + beg)
+            print("inBuff[22:24] = " + inBuff[22:25].decode("utf-8"))
+            print("Wrong data")
+        else:
+    
+            #   keTreal,      //   lTemperatureReal,
+            #   keTset,       //   lTemperatureSet,
+            #   keTcurSet,    //   lTemperatureCurrentSet,
+            #   keDeltaT,     //   lDelta_T,
+            #   keDeltat,     //   lDelta_t,
+            #   keKprop,      //   lKprop,
+            #//   keKint,       //   lKint,
+            #   keKdiff,      //   lKdiff,
+            #   keUreal,       //  sSensorData.iUcurrent,
+            #   keMaxVariableNum
+            
+            # Treal set        
+            self.Treal = (inBuff[4]  << 8) + inBuff[3]
+            print("Treal =" + format(self.Treal/100, ".2f"))
+            # Tset set ---------------------------------------------------------------------------------------------------------------------
+            self.Tset = (inBuff[6]  << 8) + inBuff[5]
+            print("Tset =" + format(self.Tset/100, ".2f"))
+            # Tcur_set set ---------------------------------------------------------------------------------------------------------------------
+            self.Tcur_set = (inBuff[8]  << 8) + inBuff[7]
+            print("Tcur_set =" + format(self.Tcur_set/100, ".2f"))
+            # D_T ---------------------------------------------------------------------------------------------------------------------
+            self.D_T = (inBuff[10]  << 8) + inBuff[9]
+            print("D_T = " + format(self.D_T/100, ".2f"))
+            # D_t ---------------------------------------------------------------------------------------------------------------------
+            self.D_t = (inBuff[12]  << 8) + inBuff[11]
+            print("D_t = " + format(self.D_t/1000, ".2f"))
+            # Kprop ---------------------------------------------------------------------------------------------------------------------
+            self.Kprop = (inBuff[14]  << 8) + inBuff[13]
+            print("Kprop =" + format(self.Kprop, "d"))
+            # Kdiff ---------------------------------------------------------------------------------------------------------------------
+            self.Kdiff = (inBuff[16]  << 8) + inBuff[15]
+            print("Kdiff =" + format(self.Kdiff, "d"))
+            # Ureal ---------------------------------------------------------------------------------------------------------------------
+            self.Ureal = (inBuff[19]  << 16) +(inBuff[18]  << 8) + inBuff[17]
+            print("Ureal =" + format(self.Ureal/1000000, ".5f"))
+            # ---------------------------------------------------------------------------------------------------------------------
+            self.SetOrScanState = inBuff[20]
+            if (self.SetOrScanState > 0):
+                print("Scan mode")
+            else:
+                print("Set mode")
+            # ---------------------------------------------------------------------------------------------------------------------
+            self.CoProcessor = inBuff[21]
+            print("Co processor states =" + "{0:b}".format(self.CoProcessor))
+# -----------------------------------------------------------------------------------
+    def VarsUpdate(self, COMConnection):
+        if (not COMConnection.isOpen()):
+            return FALSE
+        else:
+            try:
+                data = [ord('b'),ord('e'),ord('g'),254,0,0,0,ord('e'),ord('n'),ord('d')]
+                COMConnection.write(data)
+             
+                out = []
+                # let's wait one until the buffer fulfiling
+                LimitCounter = 0
+                while (COMConnection.inWaiting() < self.ReadBufferLength) and (LimitCounter < 100):
+                    time.sleep(0.1)
+                    LimitCounter = LimitCounter + 1 
+                
+                if (LimitCounter >= 99):
+                    UnitEvailable = False
+                    return False
+                
+                # All string recept   
+                while COMConnection.inWaiting() > 0:
+                    out += COMConnection.read(1)
+                   
+                if out != '':
+                    print(out)
+                    self.receivedProcessing( out)
+                UnitEvailable = True
+                return True
+            except Exception as e:                
+                print("Can't set COM Port:{}\n".format(str(e)))
+                UnitEvailable = False
+                return False
+    
+    def SendCommand(self, Command, Value, COMConnection):
+        if (not COMConnection.isOpen()):
+            return FALSE
+        else:
+            data = [ord('b'),ord('e'),ord('g'),Command,0,0,0,ord('e'),ord('n'),ord('d')]
+            WorkByte = Value & 0xFF
+            data[4] = WorkByte
+            WorkByte = (Value >> 8) & 0xFF
+            data[5] = WorkByte
+            COMConnection.write(data)
+            
+
+    def RemoteSetValue(self, Variable, StrValue, COMConnection):
+        keTset_input             = 2
+        keTstep_input            = 4
+        ketime_step_input        = 5
+        keKprop_input            = 6
+        keKdiff_input            = 7
+        keSet_ScanSelect         = 9
+        keTemperatureUnitSwitch  = 11
+        keSaveConfigs            = 12
+        keADCCalibration         = 13
+        keShowSensor             = 14
+        keNop                    = 254  # Nothing to do
+        keExit                   = 255
+        keUnknownCommand         = 0xFFFF
+
+        if Variable=="treal":
+            #case :
+            print(Variable)
+            print(StrValue)
+        elif Variable=="tset":
+            #case 
+            print(Variable)
+            print(StrValue)
+            self.SendCommand(keTset_input, int(StrValue), COMConnection)
+        elif Variable=="tcur_set":
+            #case 
+            print(Variable)
+            print(StrValue)
+        elif Variable=="d_T":
+            #case 
+            print(Variable)
+            print(StrValue)
+            self.SendCommand(keTstep_input, int(StrValue), COMConnection)
+        elif Variable=="d_t":
+            #case 
+            print(Variable)
+            print(StrValue)
+            self.SendCommand(ketime_step_input, int(StrValue), COMConnection)
+        elif Variable=="kprop":
+            #case 
+            print(Variable)
+            print(StrValue)
+            self.SendCommand(keKprop_input, int(StrValue), COMConnection)
+        elif Variable=="kdiff":
+            #case 
+            print(Variable)
+            print(StrValue)
+            self.SendCommand(keKdiff_input, int(StrValue), COMConnection)
+        elif Variable=="ureal":
+            print(Variable)
+            print(StrValue)
+        else:
+            print(Variable)
+            print(StrValue)
+        
--- src_Example/VisualExample.py
+++ src_Example/VisualExample.py
@@ -7,13 +7,29 @@ Created on 11 січ. 2022
 # Python 3+
 import tkinter as tk
 from tkinter import ttk
+from tkinter import *
+from msilib.schema import CheckBox
 
 class SampleApp(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
-        self.minsize(300, 100)
-        self.button = ttk.Button(self, text="Call toplevel!", command=self.Create_InitCommunication)
+        self.minsize(500, 500)
+        self.button = ttk.Button(self, text="Call toplevel!", command=lambda: self.SetOrScanState.set(not self.SetOrScanState.get()))
         self.button.pack(side="top")
+
+        self.SetOrScanState = IntVar()
+        self.SetOrScanState.set(1)
+        self.SetScanSelection_Rb1 = Radiobutton(self, text = "Set", 
+                                   variable = self.SetOrScanState, value = 0, command=lambda: print(self.SetOrScanState.get()))
+        self.SetScanSelection_Rb1.place(x=20,y=20)
+        
+        self.SetScanSelection_Rb2 = Radiobutton(self, text = "Scan", 
+                                   variable = self.SetOrScanState, value = 1,  command=lambda: print(self.SetOrScanState.get()))
+        self.SetScanSelection_Rb2.place(x=20,y=40)
+
+        CheckBoxExample = Checkbutton(self, variable = self.SetOrScanState)
+        CheckBoxExample.place(x=40,y=20)
+
 
     def Create_InitCommunication(self):
 
@@ -22,22 +38,18 @@ class SampleApp(tk.Tk):
 
         # Creating the toplevel dialog
         self.toplevel_dialog = tk.Toplevel(self)
-        self.toplevel_dialog.minsize(300, 100)
+        self.toplevel_dialog.minsize(600, 400)
 
         # Tell the window manager, this is the child widget.
         # Interesting, if you want to let the child window 
         # flash if user clicks onto parent
         self.toplevel_dialog.transient(self)
 
-
-
         # This is watching the window manager close button
         # and uses the same callback function as the other buttons
         # (you can use which ever you want, BUT REMEMBER TO ENABLE
         # THE PARENT WINDOW AGAIN)
         self.toplevel_dialog.protocol("WM_DELETE_WINDOW", self.Close_InitCommunication)
-
-
 
         self.toplevel_dialog_label = ttk.Label(self.toplevel_dialog, text='Do you want to enable my parent window again?')
         self.toplevel_dialog_label.pack(side='top')
@@ -47,6 +59,7 @@ class SampleApp(tk.Tk):
 
         self.toplevel_dialog_no_button = ttk.Button(self.toplevel_dialog, text='No')
         self.toplevel_dialog_no_button.pack(side='right', fill='x', expand=True)
+        
 
     def Close_InitCommunication(self):
 
